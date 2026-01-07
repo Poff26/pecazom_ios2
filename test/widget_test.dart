43686f74a7bce3b618ace8dcac18c5319cd14321
Poff26 <85941157+Poff26@users.noreply.github.com>
2026-01-07 23:41:59 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fishing_app_flutter/main.dart';
 
-import 'package:fishing_app_new/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
