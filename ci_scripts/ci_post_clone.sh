3b0f607f9b2660219661f55c5a4f81aa88030816
Poff26 <85941157+Poff26@users.noreply.github.com>
2026-01-08 20:57:39 +0100

    Update ci_post_clone.sh

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+set -e
+
+echo "CI post-clone started"
+
+cd "$CI_WORKSPACE"
+
+echo "Installing Flutter deps"
+flutter pub get
+
+echo "Precache iOS"
+flutter precache --ios
+
+echo "Installing CocoaPods"
+cd ios
+pod install
+cd ..
+
+echo "CI post-clone finished"
 
